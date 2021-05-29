e0af78956be838c0b8d81c35a5de67305e4f72d7
enrique <enrique_11_3@hotmail.com>
2021-05-29 17:01:37 -0400

    Navegacion terminada

---
+++ lib/screens/works_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WorksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Works page'),
+      ),
+    );
+  }
+}
--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -1,27 +1,61 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:get/get.dart';
 import 'package:ingemec/controllers/page_controller.dart';
-import 'package:ingemec/screens/client_screen.dart';
-import 'package:ingemec/screens/home_screen.dart';
-import 'package:ingemec/widgets/menu_item.dart';
+import 'package:ingemec/routes.dart';
+
 
 class Menu extends StatelessWidget {
-
-  final PageGetXController pageX = Get.put(PageGetXController());
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        MenuItem(icon: FeatherIcons.home,text: 'Inicio',onTap: () => pageX.currentPage = HomeScreen(),),
-        MenuItem(icon: FeatherIcons.user,text: 'Clientes',onTap: () => pageX.currentPage = ClientsScreen()),
-        MenuItem(icon: FeatherIcons.clipboard,text: 'Trabajos',),
-        MenuItem(icon: FeatherIcons.briefcase,text: 'Servicios',),
-        MenuItem(icon: FeatherIcons.users,text: 'Personal',),
-        MenuItem(icon: FeatherIcons.settings,text: 'Configuracion',),
-      ],
+      children: pageRoutesSPA.map((page) => MenuItem(routeSPA: page,)).toList(),
     );
   }
+}
+
+
+
+class MenuItem extends StatelessWidget {
+
+final RouteSPA routeSPA;
+
+MenuItem({this.routeSPA});
+
+final PageGetXController pageX = Get.put(PageGetXController());
+
+
+@override
+Widget build(BuildContext context) {
+  return GestureDetector(
+    onTap: () => pageX.changePage(this.routeSPA),
+    child: GetBuilder<PageGetXController>(
+      init: PageGetXController(),
+      id: 'page',
+      builder: (controller) {
+        return Container(
+          width: 110,
+          constraints: BoxConstraints(
+              maxHeight: 100,
+              minHeight: 80
+          ),
+          decoration: controller.currentPageIndex==this.routeSPA.index ? BoxDecoration(
+              color: Get.theme.cardColor,
+              borderRadius: BorderRadius.circular(10)
+          ) : null,
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(this.routeSPA.icon,size: 56,),
+                Text(this.routeSPA.text,style: TextStyle(color: Get.theme.primaryColor,fontSize: 16),)
+              ],
+            ),
+          ),
+        );
+      }
+    ),
+  );
+}
 }
