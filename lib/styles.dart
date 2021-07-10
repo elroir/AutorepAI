2af774218b657846cf87c52c3cb2cee5db068337
enrique <enrique_11_3@hotmail.com>
2021-07-10 14:30:32 -0400

    ios

--- lib/styles.dart
+++ lib/styles.dart
@@ -6,22 +6,20 @@ class Themes {
 
   static ThemeData themeData() {
     return ThemeData(
-      primaryColor: Colors.white,
-      // primaryColor: Color(0xFFDC4D4E),
-      // primaryColorDark: Color(0xFF071223),
-      primaryColorDark: Colors.white,
-      backgroundColor: Colors.white,
-      // backgroundColor: Color(0xFF122034),
-      // cardColor: Color(0xFF192C47),
-      cardColor: Colors.white,
+      primaryColor: Color(0xFFDC4D4E),
+      primaryColorDark: Color(0xFF071223),
+      backgroundColor: Color(0xFF122034),
+      cardColor: Color(0xFF192C47),
       iconTheme: IconThemeData(size: 30, color: Color(0xFFDC4D4E)),
-      scaffoldBackgroundColor: Colors.white,
-      // scaffoldBackgroundColor: Color(0xFF122034),
+      scaffoldBackgroundColor: Color(0xFF122034),
       canvasColor: Color(0xFF071223),
       appBarTheme: AppBarTheme(
         backgroundColor: Colors.transparent,
         elevation: 0,
         iconTheme: IconThemeData(color: Color(0xFFDC4D4E))
+      ),
+      textTheme: TextTheme(
+        bodyText2: TextStyle(color: Colors.white)
       ),
       fontFamily: 'Roboto'
 
