2c48195d0b02f6bfe7c61536265033abc0c8b588
Alejandra Murillo <alemurillo104@gmail.com>
2021-08-13 18:42:49 -0400

    ale commit

--- lib/models/cotizacion_model.dart
+++ lib/models/cotizacion_model.dart
@@ -30,7 +30,7 @@ class Cotizacion {
   int tiempoTrabajo;
   bool aprobado;
   bool estado;
-  String idUsuario; //lol cambiar a int ash
+  String idUsuario; //lol cambiar a int 
   int idVehiculo;
   Vehicle vehiculo;
   List<QuoteDetail> detalles;
