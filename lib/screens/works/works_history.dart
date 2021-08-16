ff7f2f5b91a775cda350697a4d1a5e7511025166
elroir <enrique_11_3hotmail.com>
2021-08-16 16:56:37 -0400

    F

--- lib/screens/works/works_history.dart
+++ lib/screens/works/works_history.dart
@@ -15,35 +15,35 @@ class WorksHistory extends StatelessWidget {
           id: 'history',
           builder: (controller) {
             if (!controller.loadingHistory){
-              return Column(
-                children: [
-                  SafeArea(child: SizedBox()),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:12),
-                    child: TitleWithRefreshButton(
-                      title: 'Historial',
-                      onPressed: controller.loadHistory,
+              return SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  children: [
+                    SafeArea(child: SizedBox()),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:12),
+                      child: TitleWithRefreshButton(
+                        title: 'Historial',
+                        onPressed: controller.loadHistory,
+                      ),
                     ),
-                  ),
-                  (Get.width>600)
-                      ? this._largeScreens(controller)
-                      : SingleChildScrollView(
-                      physics: BouncingScrollPhysics(),
-                      child: Wrap(
-                          alignment: WrapAlignment.center,
-                          children: [
-                            SizedBox(width: double.infinity,),
-                            ...controller.history.map((history) =>
-                                FadeInRight(duration: Duration(milliseconds: 700),child: Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: WorkItem(order: history,onTap: (){
-                                    Get.snackbar("Encargado", history.personal.nombre);
-                                  },),
-                                ))).toList()
-                          ]
-                      )
-                  )
-                ] ,
+                    (Get.width>600)
+                        ? this._largeScreens(controller)
+                        : Wrap(
+                            alignment: WrapAlignment.center,
+                            children: [
+                              SizedBox(width: double.infinity,),
+                              ...controller.history.map((history) =>
+                                  FadeInRight(duration: Duration(milliseconds: 700),child: Padding(
+                                    padding: const EdgeInsets.all(15.0),
+                                    child: WorkItem(order: history,onTap: (){
+                                      Get.snackbar("Encargado", history.personal.nombre);
+                                    },),
+                                  ))).toList()
+                            ]
+                        )
+                  ] ,
+                ),
               ) ;
             }
             return Center(child: CircularProgressIndicator());
